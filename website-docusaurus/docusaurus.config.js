31bce5ba7e2c25ed9fcacf3d561f794597576c70
Taoh Green <taohgreen@gmail.com>
2022-07-07 17:12:04 -0700

    Add deploy action

--- website-docusaurus/docusaurus.config.js
+++ website-docusaurus/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'FSML.org',
+  tagline: '',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: process.env.DOCUSAURUS_PATH_PREFIX || '/',
   trailingSlash: true,
@@ -17,8 +17,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'TeselaGen',
+  projectName: 'fsml.org', // Usually your repo name.
+  deploymentBranch: 'main',
   staticDirectories: ['static', 'public'],
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
