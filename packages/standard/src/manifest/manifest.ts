4b079af256fbd8aed6f5105ba64638979c5e3a3c
tgadam <42746585+tgadam@users.noreply.github.com>
2022-07-20 16:20:57 -0400

    refactor manifest schema to stub out for fermentation data (#76)

--- packages/standard/src/manifest/manifest.ts
+++ packages/standard/src/manifest/manifest.ts
@@ -1,6 +1,7 @@
 import { Type, Static } from '../deps/typebox.ts';
-import { ContentReference } from "./content-reference.ts";
-import { UtilityScoreDetails } from "./utility-score-details.ts";
+import { ContentReference } from './content-reference.ts';
+import { SupplementalData } from './supplemental-data.ts';
+import { SupplementalProtocol } from './supplemental-protocol.ts';
 
 export const Manifest = Type.Object({
   id: Type.String(),
@@ -9,24 +10,8 @@ export const Manifest = Type.Object({
   identifierAuthority: Type.String(),
   identifierEncoding: Type.Optional(Type.String()),
   sourceContent: Type.Array(ContentReference),
-  utilityScore: Type.Number(), // normalized to be between 0 and 1 inclusive
-  categoryScores: UtilityScoreDetails,
 
-  //supplemental sections
-  provenance: Type.Optional(Type.Any()),
-  classification: Type.Optional(Type.Any()),
-  usage: Type.Optional(Type.Any()),
-  executions: Type.Optional(Type.Any()),
-  references: Type.Optional(Type.Any()),
-  metadata: Type.Optional(Type.Any()),
-  executionContext: Type.Optional(Type.Any()),
-  protocolBlocks: Type.Optional(Type.Any()),
-  operations: Type.Optional(Type.Any()),
-  workflow: Type.Optional(Type.Any()),
-  parameters: Type.Optional(Type.Any()),
-  domainDictionary: Type.Optional(Type.Any()),
-  keywords: Type.Optional(Type.Any()),
-
+  supplementalInfo: Type.Union([SupplementalData, SupplementalProtocol]),
 });
 
 export type Manifest = Static<typeof Manifest>;
