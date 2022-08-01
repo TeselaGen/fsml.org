a1cbbed7ce8430aa8d8ce9d0c3a9e8fb25fbe6b6
Andrés Pérez Manríquez <apandresipm@gmail.com>
2022-08-01 13:25:12 -0400

    format fixes

--- app/cli/src/commands/manifest/index.ts
+++ app/cli/src/commands/manifest/index.ts
@@ -1,15 +1,49 @@
-async function create({ type, parser, format, write, pack, author, filepattern }) { }
-async function update({ type, parser, format, write, pack, patch, patchFile, patchType author, path }) { }
-async function fetch({ registry: { name, uri }, format, unpack, write, id }) { }
-async function import({ from, to, id}) { }
-async function describe({ output, write, select, summary, section, path }) { }
-async function score({ formula, dryRun, author, path }) { }
-async function export ({ exporter: { name, flags }, write, path}) { }
-async function validate({ path }) { }
-async function pack({ pack, path })
-async function unpack({ path })
+async function create(
+  { type, parser, format, write, pack, author, filepattern },
+) {}
 
+async function update(
+  {
+    type,
+    parser,
+    format,
+    write,
+    pack,
+    patch,
+    patchFile,
+    patchType,
+    author,
+    path,
+  },
+) {}
 
-const manifest = {};
+async function fetch({ registry: { name, uri }, format, unpack, write, id }) {}
 
-export default manifest
+async function _import({ from, to, id }) {}
+
+async function describe({ output, write, select, summary, section, path }) {}
+
+async function score({ formula, dryRun, author, path }) {}
+
+async function _export({ exporter: { name, flags }, write, path }) {}
+
+async function validate({ path }) {}
+
+async function pack({ pack, path }) {}
+
+async function unpack({ path }) {}
+
+const manifest = {
+  create,
+  update,
+  fetch,
+  import: _import,
+  describe,
+  score,
+  export: _export,
+  validate,
+  pack,
+  unpack,
+};
+
+export default manifest;
--- app/cli/src/deps.ts
+++ app/cli/src/deps.ts
@@ -1,19 +1,18 @@
 // This brings just the necessary lodash functions needed instead of pulling the whole package.
-import get from 'https://deno.land/x/lodash@4.17.15-es/get.js';
-import set from 'https://deno.land/x/lodash@4.17.15-es/set.js';
-import unset from 'https://deno.land/x/lodash@4.17.15-es/unset.js';
+import get from "https://deno.land/x/lodash@4.17.15-es/get.js";
+import set from "https://deno.land/x/lodash@4.17.15-es/set.js";
+import unset from "https://deno.land/x/lodash@4.17.15-es/unset.js";
 
 import yargs from "https://deno.land/x/yargs@v17.5.1-deno/deno.ts";
 
-
-export * as yaml from 'https://deno.land/std@0.82.0/encoding/yaml.ts';
-export * as path from "https://deno.land/std@0.149.0/path/mod.ts";
+export * as yaml from "https://deno.land/std@0.82.0/encoding/yaml.ts";
+// export * as path from "https://deno.land/std@0.149.0/path/mod.ts";
 export * as denoTypes from "https://deno.land/x/yargs@v17.5.1-deno/deno-types.ts";
 
 const lodash = {
-    get,
-    set,
-    unset
-}
+  get,
+  set,
+  unset,
+};
 
-export { yargs, lodash }
+export { lodash, yargs };
