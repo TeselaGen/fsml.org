008c2bfba59047c83e0f9675068067eb3de11f43
Taoh Green <taohgreen@gmail.com>
2022-08-16 16:16:54 -0700

    fix path

--- packages/standard/generate-schema.sh
+++ packages/standard/generate-schema.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-deno run --allow-read --allow-env ./example/example1.ts > ./fsml-schema.json && cat ./fsml-schema.json
+deno run --allow-read --allow-env ./example/generateSchema.ts > ./fsml-schema.json && cat ./fsml-schema.json
