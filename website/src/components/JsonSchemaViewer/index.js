49f503fe03c75c16d773df77165889066a0dfcef
Taoh Green <taohgreen@gmail.com>
2022-08-16 16:31:55 -0700

    fix schema viewer to be browser only

--- website/src/components/JsonSchemaViewer/index.js
+++ website/src/components/JsonSchemaViewer/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import SchemaViewer from 'material-ui-json-schema-viewer';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 import fsmlSchema from './fsml.schema.json';
 
-
-const myTheme= createMuiTheme({
-  palette:{
-    type: 'dark'
-  }
-});
-
 export default function JsonSchemaViewer() {
-  return (
-    <ThemeProvider theme={myTheme}>
-      <SchemaViewer
-        schema={fsmlSchema}
-      />
-    </ThemeProvider>
-  );
+  return <BrowserOnly fallback={<div>Loading...</div>}>
+    {() => {
+      const { createMuiTheme, ThemeProvider } = require('@material-ui/core/styles');
+      const SchemaViewer = require('material-ui-json-schema-viewer').default
+      const myTheme= createMuiTheme({
+        palette:{
+          type: 'dark'
+        }
+      });
+
+      return (
+        <ThemeProvider theme={myTheme}>
+          <SchemaViewer
+            schema={fsmlSchema}
+          />
+        </ThemeProvider>
+      );
+    }}
+  </BrowserOnly>
 }
