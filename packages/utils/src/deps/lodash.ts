2a8b8952cf40c1570a7bff662ad81fe90ee8d3b2
Andrés Pérez Manríquez <apandresipm@gmail.com>
2022-09-12 18:15:00 -0300

    Adds common deps to common utils package

---
+++ packages/utils/src/deps/lodash.ts
@@ -0,0 +1,23 @@
+// This brings just the necessary lodash functions needed instead of pulling the whole package.
+import identity from "https://deno.land/x/lodash@4.17.15-es/identity.js";
+import get from "https://deno.land/x/lodash@4.17.15-es/get.js";
+import set from "https://deno.land/x/lodash@4.17.15-es/set.js";
+import unset from "https://deno.land/x/lodash@4.17.15-es/unset.js";
+import isNil from "https://deno.land/x/lodash@4.17.15-es/isNil.js";
+import merge from "https://deno.land/x/lodash@4.17.15-es/merge.js";
+import omitBy from "https://deno.land/x/lodash@4.17.15-es/omitBy.js";
+import isUndefined from "https://deno.land/x/lodash@4.17.15-es/isUndefined.js";
+import uniqBy from "https://deno.land/x/lodash@4.17.15-es/uniqBy.js";
+import find from "https://deno.land/x/lodash@4.17.15-es/find.js";
+export {
+  find,
+  get,
+  identity,
+  isNil,
+  isUndefined,
+  merge,
+  omitBy,
+  set,
+  uniqBy,
+  unset,
+};
