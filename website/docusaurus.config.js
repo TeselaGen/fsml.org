28e93908e7064f4b06c33b8b04f526331c96ac21
Eduardo Abeliuk <eabeliuk@gmail.com>
2022-10-29 18:39:57 -0400

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'FSML.org',
+  title: 'FSML',
   tagline: '',
   url: 'https://fsml.org',
   baseUrl: process.env.DOCUSAURUS_PATH_PREFIX || '/',
